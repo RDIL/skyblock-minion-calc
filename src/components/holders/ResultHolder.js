8d11f8d1c41734acc4b99911f601606114aa52c8
Reece Dunham <me@rdil.rocks>
2019-10-31 14:45:01 +0000

    missed some files

---
+++ src/components/holders/ResultHolder.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2019-present Reece Dunham
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react"
+import ReactMarkdown from "react-markdown"
+
+export default props => {
+    let base = `## Result\n\nIt will take ${props.meta[6]} raw items to make that minion.\n\n### Enchantments\n\n`
+    // divide total cost by calculated price per enchanted item
+    let cost = props.meta[6] / props.meta[0]
+    let b1 = cost % 1 == 0
+    if (props.meta[2] && b1) {
+        base +=
+            `In terms of enchanted items, it will cost ${cost}.` +
+            (props.meta[1]
+                ? `\n\n**Warning**: This item has an unusual enchanted item cost, and depending on the item may require more or less of the item to craft the enchanted form!`
+                : "")
+    } else if (props.meta[2] && !b1) {
+        base += "Failed to calculate because the number of enchanted items had decimals!"
+    } else {
+        base += "The specified tier does not require enchanted items."
+    }
+
+    return (
+        <div>
+            <ReactMarkdown source={base} />
+        </div>
+    )
+}
